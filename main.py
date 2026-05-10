fd168a8d89e8d39518eb18b64b1bac6fe2ac5755
Samsons Rostovs <samsons.rostovs@gmail.com>
2026-05-10 16:24:38 +0200

    Simulation execution according to the transition function

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
+from tm.machine import TuringMachine, Transition
 from tm.simulator import TuringMachineSimulator
 from visualization.tape_renderer import render_configuration
 
 
-simulator = TuringMachineSimulator()
+transitions = {
+    ("q0", "1"): Transition(
+        next_state="q1",
+        write_symbol="1",
+        move_direction="R"
+    ),
 
-simulator.add_configuration(
-    state="q0",
-    tape=["1", "0", "1", "_"],
-    head_position=0,
-    step=0
+    ("q1", "0"): Transition(
+        next_state="q_accept",
+        write_symbol="0",
+        move_direction="R"
+    )
+}
+
+
+machine = TuringMachine(
+    states={"q0", "q1", "q_accept", "q_reject"},
+
+    input_alphabet={"0", "1"},
+
+    tape_alphabet={"0", "1", "_"},
+
+    transitions=transitions,
+
+    start_state="q0",
+
+    accept_state="q_accept",
+
+    reject_state="q_reject"
 )
 
-simulator.add_configuration(
-    state="q1",
-    tape=["1", "0", "1", "_"],
-    head_position=1,
-    step=1
-)
+
+simulator = TuringMachineSimulator(machine)
+
+simulator.simulate("10")
+
 
 for configuration in simulator.get_history():
     print(render_configuration(configuration))
--- tm/simulator.py
+++ tm/simulator.py
@@ -1,9 +1,57 @@
 from tm.configuration import Configuration
+from tm.machine import TuringMachine
+
 
 class TuringMachineSimulator:
 
-    def __init__(self):
+    def __init__(self, machine: TuringMachine):
+
+        self.machine = machine
         self.history: list[Configuration] = []
+
+    def simulate(self, input_word: str, max_steps: int = 100):
+
+        tape = list(input_word) + ["_"]
+        current_state = self.machine.start_state
+        head_position = 0
+        step = 0
+
+        self.add_configuration(
+            current_state,
+            tape,
+            head_position,
+            step
+        )
+
+        while (
+            current_state != self.machine.accept_state
+            and current_state != self.machine.reject_state
+            and step < max_steps
+        ):
+            current_symbol = tape[head_position]
+            transition_key = (current_state, current_symbol)
+
+            if transition_key not in self.machine.transitions:
+                break
+
+            transition = self.machine.transitions[transition_key]
+            tape[head_position] = transition.write_symbol
+            current_state = transition.next_state
+
+            if transition.move_direction == "R":
+                head_position += 1
+                if head_position >= len(tape):
+                    tape.append("_")
+            elif transition.move_direction == "L":
+                head_position = max(0, head_position - 1)
+            
+            step += 1
+            self.add_configuration(
+                current_state,
+                tape,
+                head_position,
+                step
+            )
 
     def add_configuration(
             self,
